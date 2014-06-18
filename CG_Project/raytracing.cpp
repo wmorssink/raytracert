a3905cab7cd82d146a5ac021e4a68c0ccc768eb6
Lars den Herder <ldherder@gmail.com>
2014-06-18 12:12:51 +0200

    Fixed error and moved to cpp

--- CG_Project/raytracing.cpp
+++ CG_Project/raytracing.cpp
@@ -58,8 +58,57 @@ void yourDebugDraw()
 
 }
 
-void yourKeyboardFunc(char t, int x, int y)
-{
+void yourKeyboardFunc(char key, int x, int y){
+    
+    switch(key){
+        case '1':
+            Ambient=!Ambient;
+            break;
+            
+        case '2':
+            Reflection=!Reflection;
+            break;
+            
+        case '3':
+            Shadows=!Shadows;
+            break;
+            
+        case '4':
+            Specular=!Specular;
+            break;
+    }
+    
+    // Activate ambient lighting.
+    if(Ambient){
+        std::cout<<std::endl<<("DEBUG --- Ambient on")<<std::endl;
+    }
+    else{
+        std::cout<<std::endl<<("DEBUG --- Ambient off")<<std::endl;
+    }
+    
+    // Activate reflection.
+    if(Reflection){
+        std::cout<<std::endl<<("DEBUG --- Reflection on")<<std::endl;
+    }
+    else{
+        std::cout<<std::endl<<("DEBUG --- Reflection off")<<std::endl;
+    }
+    
+    // Activate shadow.
+    if(Shadows){
+        std::cout<<std::endl<<("DEBUG --- Shadows on")<<std::endl;
+    }
+    else{
+        std::cout<<std::endl<<("DEBUG --- Shadows off")<<std::endl;
+    }
+    
+    // Activate specularity.
+    if(Specular){
+        std::cout<<std::endl<<("DEBUG --- Speculartiy on")<<std::endl;
+    }
+    else{
+        std::cout<<std::endl<<("DEBUG --- Specularity off")<<std::endl;
+    }
 	// do what you want with the keyboard input t.
 	// x, y are the screen position
 
--- CG_Project/raytracing.h
+++ CG_Project/raytracing.h
@@ -35,56 +35,6 @@ Vec3Df performRayTracing(const Vec3Df & origin, const Vec3Df & dest);
 void yourDebugDraw();
 
 //want keyboard interaction? Here it is...
-void yourKeyboardFunc(char key, int x, int y){
-    switch(key){
-            case '1':
-            Ambient=!Ambient;
-            break;
-            
-            case '2':
-            Reflection=!Reflection;
-            break;
-            
-            case '3':
-            Shadows=!Shadows;
-            break;
-        
-            case '4':
-            Specular=!Specular;
-            break;
-        }
-    
-    // Activate ambient lighting.
-    if(Ambient){
-        std::cout<<std::endl<<("DEBUG --- Ambient on")<<std::endl;
-    }
-    else{
-        std::cout<<std::endl<<("DEBUG --- Ambient off")<<std::endl;
-    }
-    
-    // Activate reflection.
-    if(Reflection){
-        std::cout<<std::endl<<("DEBUG --- Reflection on")<<std::endl;
-    }
-    else{
-        std::cout<<std::endl<<("DEBUG --- Reflection off")<<std::endl;
-    }
-    
-    // Activate shadow.
-    if(Shadows){
-        std::cout<<std::endl<<("DEBUG --- Shadows on")<<std::endl;
-    }
-    else{
-        std::cout<<std::endl<<("DEBUG --- Shadows off")<<std::endl;
-    }
-    
-    // Activate specularity.
-    if(Specular){
-        std::cout<<std::endl<<("DEBUG --- Speculartiy on")<<std::endl;
-    }
-    else{
-        std::cout<<std::endl<<("DEBUG --- Specularity off")<<std::endl;
-    }
-};
+void yourKeyboardFunc(char key, int x, int y);
 
 #endif
